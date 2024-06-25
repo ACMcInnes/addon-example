f81376ee0b9457e42dc0d8c389891ba104febe50
Andrew McInnes <acmcinnes@outlook.com>
2024-06-25 16:57:58 +1000

    testing environment variable

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -7,6 +7,7 @@ export default function About() {
   return (
     <>
       <p className="mt-4">Developed by <Link href="https://acmcinnes.au" target="_blank" className="text-sky-500">Andrew McInnes <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></Link></p>
+      <p><Link href={`//${process.env.VERCEL_PROJECT_PRODUCTION_URL}/dashboard`} className="text-sky-500">//{process.env.VERCEL_PROJECT_PRODUCTION_URL}/dashboard</Link></p>
     </>
   );
 }
