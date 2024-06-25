78a42fb31d5c949db6c242988905f79905609f02
Andrew McInnes <acmcinnes@outlook.com>
2024-06-25 17:07:44 +1000

    checking absolute redirect URL

--- app/neto/callback/route.ts
+++ app/neto/callback/route.ts
@@ -8,10 +8,11 @@ export async function GET(request: NextRequest) {
 
   if (hasWebstore) {
     const webstoreURL = searchParams.get("store_domain") as string;
-    // console.log(`store_domain: ${webstoreURL}`);
+    console.log(`redirect production url: //${process.env.VERCEL_PROJECT_PRODUCTION_URL}/dashboard`);
+    console.log(`redirect branch url: //${process.env.VERCEL_BRANCH_URL}/dashboard`);
 
     //try {
-      await signIn('neto', { callbackUrl: '/dashboard' }, { store_domain: webstoreURL });
+      await signIn('neto', { callbackUrl: `//${process.env.VERCEL_BRANCH_URL}/dashboard` }, { store_domain: webstoreURL });
 
       // console.log(`AUTH RESPONSE:`);
       // console.log(auth);
